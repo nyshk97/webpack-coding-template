60ce116a48ff9d276ebde03b1e510c6e9dd65309
tsubasa namatame <nyshk97@gmail.com>
2020-04-22 16:46:28 +0900

    Add sample form

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,6 +1,6 @@
 import '../stylesheets/style.scss';
 
-// import './lib/jquery';
+import './lib/jquery';
 // import './lib/font-awesome'
 // import swiper from './lib/swiper'
 
--- src/javascripts/lib/jquery.js
+++ src/javascripts/lib/jquery.js
@@ -1,4 +1,17 @@
 window.$ = window.jQuery = require('jquery');
-$(function() {
-  $('.main').css('color', 'Red');
+
+// $(function() {
+//   $('.main').css('color', 'Red');
+// });
+
+$(function () {
+  $(".file").on('change', function () {
+    let file = $(this).prop('files')[0];
+    let label = $(this).next();
+    if (!($(this).children(".filename").length)) {
+      $(this).parent(".form__file").append('<span class="filename"></span>');
+      label.addClass('changed');
+    }
+    label.next().html(file.name);
+  });
 });
