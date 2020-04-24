0daa226d444bed27ceecf13a7e1790c0a9bc81be
tsubasa namatame <nyshk97@gmail.com>
2020-04-24 13:39:25 +0900

    Modify swiper

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,8 +1,10 @@
 import "../stylesheets/style.scss";
 
 import "./lib/jquery";
-// import './lib/font-awesome'
-// import swiper from './lib/swiper'
-// import { sampleChart } from "./lib/chart";
 
-// swiper()
+// import './lib/font-awesome'
+
+// import { sampleSwiper } from "./lib/swiper";
+// sampleSwiper();
+
+// import { sampleChart } from "./lib/chart";
--- src/javascripts/lib/swiper.js
+++ src/javascripts/lib/swiper.js
@@ -1,19 +1,19 @@
-import Swiper from 'swiper'
-import 'swiper/css/swiper.css'
+import Swiper from "swiper";
+import "swiper/css/swiper.css";
 
 const options = {
   loop: true,
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
+    el: ".swiper-pagination",
+    type: "bullets",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-}
-const mySwiper = () => {
-  new Swiper ('.swiper-container', options)
-}
-export default mySwiper
+};
+
+export const sampleSwiper = () => {
+  new Swiper(".swiper-container", options);
+};
